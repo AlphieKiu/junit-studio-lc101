8fa0235bb361b4cb71568465c649e32ee548f8f6
AlphieKiu <kiuhuaien87@gmail.com>
2019-11-21 17:26:09 -0600

    Studio 5 done.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,70 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
+    BalancedBrackets test_Class = new BalancedBrackets();
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBrackets() {
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
     }
-
+    @Test
+    public void emptyString() {
+        assertTrue(test_Class.hasBalancedBrackets(""));
+    }
+    @Test
+    public void stringNoBrackets() {
+        assertTrue(test_Class.hasBalancedBrackets("LaunchCode"));
+    }
+    @Test
+    public void emptyBracketsBeforeString() {
+        assertTrue(test_Class.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void bracketsWrappingString() {
+        assertTrue(test_Class.hasBalancedBrackets("[LaunchCode]"));
+    }
+    @Test
+    public void bracketsPartOfString() {
+        assertTrue(test_Class.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void twoPairsOfBrackets() {
+        assertTrue(test_Class.hasBalancedBrackets("[Launch][Code]"));
+    }
+    //assertFalse
+    @Test
+    public void emptyBracketFlipOrder() {
+        assertFalse(test_Class.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void singleLeftBracket() {
+        assertFalse(test_Class.hasBalancedBrackets("["));
+    }
+    @Test
+    public void singleRightBracket() {
+        assertFalse(test_Class.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void stringLeftBracket() {
+        assertFalse(test_Class.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void stringRightBracket() {
+        assertFalse(test_Class.hasBalancedBrackets("LaunchCode]"));
+    }
+    @Test
+    public void partOfStringFlipBrackets() {
+        assertFalse(test_Class.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void flipBracketsWrappingString() {
+        assertFalse(test_Class.hasBalancedBrackets("]LaunchCode["));
+    }
 
 }
